2eca7e58d7a774ac1f8409761131847bdfa412c7
Sander <hey@sandydoo.me>
2025-06-12 00:48:00 +0200

    devenv: remove inactionable inefficient double copy warning

--- src/libexpr/paths.cc
+++ src/libexpr/paths.cc
@@ -60,11 +60,6 @@ std::string EvalState::computeBaseName(const SourcePath & path)
 {
     if (path.accessor == rootFS) {
         if (auto storePath = store->maybeParseStorePath(path.path.abs())) {
-            warn(
-                "Performing inefficient double copy of path '%s' to the store. "
-                "This can typically be avoided by rewriting an attribute like `src = ./.` "
-                "to `src = builtins.path { path = ./.; name = \"source\"; }`.",
-                path);
             return std::string(
                 fetchToStore(fetchSettings, *store, path, FetchMode::DryRun, storePath->name()).to_string());
         }
