820a5c810ebbc11d97eec6a03d7d3278507f183a
Domen Kožar <domen@cachix.org>
2025-12-10 22:01:55 +0100

    feat(libfetchers-c): auto-load nix.conf settings in nix_fetchers_settings_new
    Pre-populate fetchers settings from nix.conf files when creating a new
    settings object. This enables access-tokens and other fetcher-related
    configuration to work out of the box.
    
    Requires nix_libstore_init() to have been called first.

--- src/libfetchers-c/nix_api_fetchers.cc
+++ src/libfetchers-c/nix_api_fetchers.cc
@@ -2,12 +2,19 @@
 #include "nix_api_fetchers_internal.hh"
 #include "nix_api_util_internal.h"
 
+#include "nix/store/globals.hh"
+
 extern "C" {
 
 nix_fetchers_settings * nix_fetchers_settings_new(nix_c_context * context)
 {
     try {
-        auto fetchersSettings = nix::make_ref<nix::fetchers::Settings>(nix::fetchers::Settings{});
+        auto fetchersSettings = nix::make_ref<nix::fetchers::Settings>();
+
+        // Pre-populate settings from nix.conf files.
+        // Requires nix_libstore_init() to have been called first.
+        nix::loadConfFile(*fetchersSettings);
+
         return new nix_fetchers_settings{
             .settings = fetchersSettings,
         };
